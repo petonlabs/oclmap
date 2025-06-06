295ce9ab12c3cb5d730bd67ac3df6ca0de7797df
Sny <sny.aggarwal@gmail.com>
2025-06-06 08:19:43 +0530

    OpenConceptLab/ocl_issues#2183 | header to only have properties if they exist

--- src/components/map-projects/MappingDecisionResult.jsx
+++ src/components/map-projects/MappingDecisionResult.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Typography from '@mui/material/Typography'
 import ListItemText from '@mui/material/ListItemText'
+import has from 'lodash/has'
 import MapButton from './MapButton'
 import { URIToParentParams } from '../../common/utils'
 
 const MappingDecisionResult = ({targetConcept, row, rowIndex, mapTypes, allMapTypes, onMap}) => {
   const parentParams = targetConcept?.url ? URIToParentParams(targetConcept.url) : {}
+  const hasClass = has(row, 'Class') || has(row, 'Concept Class')
+  const hasDatatype = has(row, 'Datatype') || has(row, 'datatype')
   return (
     <div className='col-xs-12 padding-0' style={{display: 'flex', margin: '8px 0'}}>
         <Typography component='div' sx={{display: 'inline-block', color: 'surface.dark', fontWeight: 600, marginRight: '24px', fontSize: '14px'}}>Mapping</Typography>
@@ -13,11 +16,17 @@ const MappingDecisionResult = ({targetConcept, row, rowIndex, mapTypes, allMapTy
         <Typography component='span' sx={{color: 'rgba(0, 0, 0, 0.6)', fontSize: '12px'}}>Source Code</Typography>
         <div className='col-xs-12 padding-0'>
           <ListItemText
-            primary={row?.Name || row?.name}
+            primary={row?.Name || row?.name || '-'}
             secondary={
               <span style={{fontSize: '12px'}}>
-                Class: <i>{row.Class || row['Concept Class']}</i>,
-                Datatype: <i>{row.Datatype}</i>
+                {
+                hasClass &&
+                    <>Class: <i>{row.Class || row['Concept Class']}</i>,</>
+                }
+                {
+                  hasDatatype &&
+                    <>Datatype: <i>{row.Datatype}</i></>
+                }
               </span>
             }
             sx={{marginTop: 0, '.MuiListItemText-secondary': {marginTop: '-4px'}}}
@@ -35,9 +44,10 @@ const MappingDecisionResult = ({targetConcept, row, rowIndex, mapTypes, allMapTy
               <Typography component='span' sx={{color: 'rgba(0, 0, 0, 0.6)', fontSize: '12px'}}>Target Code</Typography>
               <div className='col-xs-12 padding-0'>
                 <ListItemText
+                  className='searchable'
                   primary={`${parentParams.repo}:${targetConcept.id} ${targetConcept.display_name || ''}`}
                   secondary={
-                    <span style={{fontSize: '12px'}}>
+                    <span className='searchable' style={{fontSize: '12px'}}>
                       Class: <i>{targetConcept.concept_class}</i>,
                       Datatype: <i>{targetConcept.datatype}</i>
                     </span>
