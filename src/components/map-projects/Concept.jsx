42ae084f43104c1db5ffd1e436e764deaa75d2b0
Sny <sny.aggarwal@gmail.com>
2025-08-04 22:02:14 +0530

    OpenConceptLab/ocl_issues#2183 | Search results | showing best match synonym from highlight as part of name

--- src/components/map-projects/Concept.jsx
+++ src/components/map-projects/Concept.jsx
@@ -5,6 +5,21 @@ import Retired from '../common/Retired'
 import Score from './Score'
 import MapButton from './MapButton'
 
+const getBestSynonym = synonyms => {
+  return synonyms
+    .map(text => {
+      const matches = [...text.matchAll(/<em>(.*?)<\/em>/g)];
+      const longestMatch = matches.reduce((a, b) => (b[1].length > a.length ? b[1] : a), "");
+      const startsWithMatch = text.indexOf(`<em>${longestMatch}</em>`) === 0;
+      return { text, longestMatch, length: longestMatch.length, startsWithMatch };
+    })
+    .sort((a, b) => {
+      if (b.length !== a.length) return b.length - a.length; // longest match first
+      if (b.startsWithMatch !== a.startsWithMatch) return b.startsWithMatch ? 1 : -1; // prefer start
+      return 0;
+    })[0].text; // return best match's text
+}
+
 const Concept = ({firstChild, concept, setShowHighlights, isShown, onCardClick, onMap, isSelectedForMap, noScore}) => {
   const id = concept?.version_url || concept?.url || concept?.id
   const isSelectedToShow = isShown(id)
@@ -13,8 +28,10 @@ const Concept = ({firstChild, concept, setShowHighlights, isShown, onCardClick, 
   const highlights = concept?.search_meta?.search_highlight
   const synonymHighlight = highlights?.synonyms
   const nameHighlight = highlights?.name
-  if(!nameHighlight?.length && synonymHighlight?.length)
-    synonymPrefix = synonymHighlight[0].replace('<em>', "<b className='searchable'>").replace('</em>', '</b>')
+  if(!nameHighlight?.length && synonymHighlight?.length) {
+    const bestMatch = getBestSynonym(synonymHighlight) || synonymHighlight[0]
+    synonymPrefix = bestMatch.replace('<em>', "<b className='searchable'>").replace('</em>', '</b>')
+  }
 
   return (
     <ListItemButton selected={isSelectedToShow} onClick={event => onCardClick(event, id)} sx={{padding: '8px', borderTop: firstChild ? undefined : '1px solid rgba(0, 0, 0, 0.1)'}}>
