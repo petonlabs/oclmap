0f0b052a5954db8db4ea54eff322bb55901a825e
Sny <sny.aggarwal@gmail.com>
2025-09-21 08:30:25 +0530

    OpenConceptLab/ocl_issues#2211 | removed flex from property to properly format spaces

--- src/components/concepts/Property.jsx
+++ src/components/concepts/Property.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Property = ({code, value}) => {
   return (
-    <span className='searchable' style={{marginRight: '6px', display: 'inline-flex', alignItems: 'baseline'}}><i style={{marginRight: '3px', fontSize: '10px'}}>{code}:</i>{value}</span>
+    <span className='searchable' style={{marginRight: '6px'}}><i style={{marginRight: '3px', fontSize: '10px'}}>{code}:</i>{value}</span>
   )
 }
 
