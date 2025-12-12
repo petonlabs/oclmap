bf721fbeed55056126babadf4acd34a9281298e5
Sny <sny.aggarwal@gmail.com>
2025-12-12 08:45:43 +0530

    OpenConceptLab/ocl_issues#2092 | Merging Download CSV and Save to Collection

--- src/components/map-projects/Controls.jsx
+++ src/components/map-projects/Controls.jsx
@@ -4,6 +4,10 @@ import { useTranslation } from 'react-i18next';
 import IconButton from '@mui/material/IconButton'
 import Button from '@mui/material/Button'
 import Tooltip from '@mui/material/Tooltip'
+import ListItemText from '@mui/material/ListItemText';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import ListItemIcon from '@mui/material/ListItemIcon';
 import CircularProgress from '@mui/material/CircularProgress'
 import DownloadIcon from '@mui/icons-material/Download';
 import SaveIcon from '@mui/icons-material/Save';
@@ -11,11 +15,12 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import { copyToClipboard } from '../../common/utils'
 import RepoIcon from '../repos/RepoIcon'
 
-const IkonButton = ({title, icon, onClick, color, disabled}) => {
+const IkonButton = ({title, icon, onClick, color, disabled, id}) => {
   return (
     <Tooltip title={title}>
       <span>
         <IconButton
+          id={id}
           color={color}
           size='small'
           sx={{textTransform: 'none', margin: '2px 5px'}}
@@ -31,8 +36,11 @@ const IkonButton = ({title, icon, onClick, color, disabled}) => {
 
 const Controls = ({project, onDownload, onSave, onDelete, owner, file, isSaving, onImport, importResponse, onDownloadImportReport}) => {
   const { t } = useTranslation();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const downloadOpen = Boolean(anchorEl);
   const lastSavedText = project?.updated_at ? moment(project.updated_at).fromNow() : false
   const isRunningImport = ['STARTED', 'RECEIVED', 'PENDING'].includes(importResponse?.state)
+
   return (
     <span style={{textAlign: 'right'}}>
       <div>
@@ -44,21 +52,12 @@ const Controls = ({project, onDownload, onSave, onDelete, owner, file, isSaving,
           icon={<SaveIcon />}
         />
         <IkonButton
+          id='download-button'
           color='secondary'
-          onClick={onDownload}
+          onClick={event => setAnchorEl(event.currentTarget)}
           title={t('map_project.download_this_project_as_csv')}
           icon={<DownloadIcon />}
         />
-        {
-        onImport &&
-            <IkonButton
-              color='secondary'
-              onClick={onImport}
-              disabled={isRunningImport}
-              title={t('map_project.save_this_project_to_collection')}
-              icon={isRunningImport ? <CircularProgress size={20} /> : <RepoIcon noTooltip selected />}
-            />
-        }
         {
           project?.id &&
             <IkonButton
@@ -100,6 +99,36 @@ const Controls = ({project, onDownload, onSave, onDelete, owner, file, isSaving,
             }
           </div>
       }
+
+    <Menu
+        id="download-menu"
+        anchorEl={anchorEl}
+        open={downloadOpen}
+        onClose={() => setAnchorEl(null)}
+        slotProps={{
+          list: {
+            'aria-labelledby': 'download-button',
+            role: 'listbox',
+          },
+        }}
+      >
+          <MenuItem onClick={() => {setAnchorEl(null); onDownload();}}>
+            <ListItemIcon>
+            <i className="fa-solid fa-file-csv" style={{fontSize: '1.25rem'}}></i>
+            </ListItemIcon>
+          <ListItemText>{t('common.download_csv')}</ListItemText>
+          </MenuItem>
+          {
+            onImport &&
+            <MenuItem onClick={() => {setAnchorEl(null); onImport();}} disabled={isRunningImport}>
+            <ListItemIcon>
+            <RepoIcon noTooltip />
+            </ListItemIcon>
+          <ListItemText>{t('map_project.save_to_collection')}</ListItemText>
+          {isRunningImport ? <CircularProgress sx={{marginLeft: '16px'}} size={20} /> : null}
+          </MenuItem>
+          }
+      </Menu>
     </span>
 
   )
